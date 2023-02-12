82ab0b9eaafd843bcc732b558a72e789bb7fe5af
gihun3645 <gihun3645@naver.com>
2023-02-12 12:18:45 +0900

    즐거운 자바 강좌
    자바 기본 문법 - 타입의 변환

--- src/main/java/자바_기본_문법/NumberOverflow.java
+++ src/main/java/자바_기본_문법/NumberOverflow.java
@@ -7,5 +7,10 @@ public class NumberOverflow {
 
         System.out.println(value + 1);
         System.out.println(maxInt + 1);
+
+        double d1 =50;
+        double d2 = 500L;
+        System.out.println(d1);
+        System.out.println(d2);
     }
 }
---
+++ src/main/java/자바_기본_문법/primitiveCastExam4.java
@@ -0,0 +1,15 @@
+package 자바_기본_문법;
+
+public class primitiveCastExam4 {
+    public static void main(String[] args) {
+        long x2 = 50;
+        int i2 = (int)x2;
+        System.out.println(x2);
+        System.out.println(i2);
+
+        long x3 = Long.MAX_VALUE;
+        int i3 = (int)x3;
+        System.out.println(x3);
+        System.out.println(i3);
+    }
+}
