ca0363c60f27efa1350d3bfbf3d67661481a073b
gihun3645 <gihun3645@naver.com>
2023-02-15 18:22:49 +0900

    즐거운 자바 강좌
    객체지향문법 1/3 - 자판기 클래스 만들기  & 그림으로 클래스와 관계 표현하기

---
+++ src/main/java/객체지향문법_1/VendingMachine.java
@@ -0,0 +1,8 @@
+package 객체지향문법_1;
+
+public class VendingMachine {
+    // field (가지는 것들)
+
+    // 생성자
+    // method
+}
---
+++ src/main/java/객체지향문법_1/VendingMachineMain.java
@@ -0,0 +1,7 @@
+package 객체지향문법_1;
+
+public class VendingMachineMain {
+    public static void main(String[] args) {
+        VendingMachine vm1 = new VendingMachine(); // 자판기만 메모리에 생성
+    }
+}
