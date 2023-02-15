e39d6d44b61b0fe5f0f4c7954152b9ed9ec707e2
gihun3645 <gihun3645@naver.com>
2023-02-16 07:43:21 +0900

    즐거운 자바 강좌
    자바 기본 문법 - 메소드, 메시지 교환 , 자판기에 메소드 추가하기

--- src/main/java/객체지향문법_1/VendingMachine.java
+++ src/main/java/객체지향문법_1/VendingMachine.java
@@ -5,4 +5,8 @@ public class VendingMachine {
 
     // 생성자
     // method
+    public String pushProductButton(int menuId) {
+        System.out.println(menuId+"를 전달받았습니다.");
+        return "콜라";
+    }
 }
--- src/main/java/객체지향문법_1/VendingMachineMain.java
+++ src/main/java/객체지향문법_1/VendingMachineMain.java
@@ -3,5 +3,9 @@ package 객체지향문법_1;
 public class VendingMachineMain {
     public static void main(String[] args) {
         VendingMachine vm1 = new VendingMachine(); // 자판기만 메모리에 생성
+
+        // 리턴된 "콜라"는 product 변수가 참조하게 됨
+        String product = vm1.pushProductButton(100);
+        System.out.println(product);
     }
 }
